9c84586c94dfbfce08d1b2aca784d96fe275bf2d
masayushi <4a1e0010@stust.edu.tw>
2020-07-12 13:52:53 +0800

    待修正，無法捲動至指定處

--- GO_TO_Element.js
+++ GO_TO_Element.js
@@ -1,14 +1,22 @@
 window.onload = () => {
-    console.log("頁面載入完成~");
-}
-
+    // console.log("頁面載入完成~");
+// 取得元素
 var test = document.getElementById("test");
 
+// 元素.點擊事件
 test.onclick = () => {
-   var target = test.getAttribute("data-gte-target");
-   var offset = test.getAttribute("data-gte-offset");
+   var target = test.getAttribute("data-gte-target"); // 目標屬性
+   var offset = test.getAttribute("data-gte-offset"); // 位移屬性
 
-   console.log(target);
-   console.log(offset);
-}
+//    console.log(target);
+//    console.log(offset);
+
+   var element = document.getElementById(target); //目標元素
+
+   //視窗.捲動至({上方，行為})
+   window.scrollTo({
+       top: element.offsetTop,
+       behavior: "smooth"
+      })
+    }
 }
